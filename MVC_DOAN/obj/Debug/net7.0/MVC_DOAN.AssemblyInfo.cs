5dd2a6025a8e5379263b54ec366ef49266f1f093
tntn245 <tranngoctonhu2405@gmail.com>
2023-12-12 14:32:54 +0700

    FE login

--- MVC_DOAN/obj/Debug/net7.0/MVC_DOAN.AssemblyInfo.cs
+++ MVC_DOAN/obj/Debug/net7.0/MVC_DOAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_DOAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a27896bc50b87ead558696fb59536959b55e88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d1dfd86461fd243e9513f9852e109697d1b359")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_DOAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_DOAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
